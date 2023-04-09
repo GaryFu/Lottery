88fb5df71792599c711e228f6f08fecb09032ab1
Gary Fu <fuguangji@wanda.cn>
2023-04-09 21:58:32 +0800

    增加移动浏览器上双击事件处理

--- src/components/Lottery.js
+++ src/components/Lottery.js
@@ -28,7 +28,8 @@ function Lottery({ maxNumber, digits }) {
 
   useEffect(() => {
     const handleKeyPress = (e) => {
-      if (e.key === 'Enter') {
+      if (e.code === 'Enter' || e.code === 'Space') {
+        e.preventDefault();
         setIsRolling(!isRolling);
         if (!isRolling) {
           const drawnNumber = Math.floor(Math.random() * maxNumber) + 1;
@@ -43,6 +44,44 @@ function Lottery({ maxNumber, digits }) {
       window.removeEventListener('keypress', handleKeyPress);
     };
   }, [isRolling, maxNumber, digits]);
+
+ useEffect(() => {
+    const handleDoubleClick = (e) => {
+      e.preventDefault();
+      setIsRolling(!isRolling);
+      if (!isRolling) {
+        const drawnNumber = Math.floor(Math.random() * maxNumber) + 1;
+        const numberString = drawnNumber.toString().padStart(digits, '0');
+        setFixedDigits(numberString.split(''));
+      }
+    };
+    window.addEventListener('dblclick', handleDoubleClick);
+    return () => {
+      window.removeEventListener('dblclick', handleDoubleClick);
+    };
+  }, [isRolling, maxNumber, digits]);
+
+ useEffect(() => {
+    const handleTouchStart = (e) => {
+      e.preventDefault();
+      setIsRolling(!isRolling);
+      const currentTime = new Date().getTime();
+      const timeDiff = currentTime - (lastTouch || currentTime);
+      if (timeDiff < 300) {
+        if (!isRolling) {
+          const drawnNumber = Math.floor(Math.random() * maxNumber) + 1;
+          const numberString = drawnNumber.toString().padStart(digits, '0');
+          setFixedDigits(numberString.split(''));
+        }
+      }
+      setLastTouch(currentTime);
+    };
+    window.addEventListener('touchstart', handleTouchStart);
+    return () => {
+      window.removeEventListener('touchstart', handleTouchStart);
+    };
+  }, [isRolling, maxNumber, digits]);
+
   const spheres = isRolling ? randomDigits : fixedDigits;
 
   return (
