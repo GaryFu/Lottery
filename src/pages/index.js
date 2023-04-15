c94eced12f1e80a26d8505798ab70ed989694adc
Gary Fu <fuguangji@wanda.cn>
2023-04-15 21:27:00 +0800

    count down

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Lottery from '../components/Lottery';
 import styles from '../components/Lottery.module.css';
 
 function Home() {
-  const router = useRouter();
+    const router = useRouter();
+    const [daysUntilExam, setDaysUntilExam] = useState(0);
 
-  const handleClick = () => {
-    router.push('/group-draw');
-  };
+    useEffect(() => {
+        const days = getDaysUntilExam();
+        setDaysUntilExam(days);
+    }, []);
 
-  return (
-    <div>
-      <h1 className={styles.title}>学    号</h1>
-      <Lottery maxNumber={38} digits={2} />
-      <div>
+    const handleClick = () => {
+        router.push('/group-draw');
+    };
+
+    const getDaysUntilExam = () => {
+        const currentDate = new Date();
+        const examDate = new Date(2023, 5, 17); // June 17, 2023
+        const timeDifference = examDate - currentDate;
+        const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+        return daysDifference;
+    };
+
+    return (
+        <div>
+            <h1 className={styles.title}>学号</h1>
+            <Lottery maxNumber={38} digits={2} />
+            <div>
         <span className={styles.navLink} onClick={handleClick}>
-          去抽一个小组
+          Go to Group Draw
         </span>
-      </div>
-    </div>
-  );
+            </div>
+            <div className={styles.floatingText}>离中考还有 {daysUntilExam} 天</div>
+        </div>
+    );
 }
 
 export default Home;
