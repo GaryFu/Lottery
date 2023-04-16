028c797830df8ccd39d29323024f34466c36ef67
Gary Fu <fuguangji@wanda.cn>
2023-04-16 11:35:18 +0800

    drawed list

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Lottery from '../components/Lottery';
 import styles from '../components/Lottery.module.css';
-
 function Home() {
     const router = useRouter();
     const [daysUntilExam, setDaysUntilExam] = useState(0);
+    const [drawCounts, setDrawCounts] = useState(Array(38).fill(0));
 
     useEffect(() => {
         const days = getDaysUntilExam();
@@ -24,16 +24,34 @@ function Home() {
         return daysDifference;
     };
 
+    const handleDraw = (number) => {
+        const newDrawCounts = [...drawCounts];
+        newDrawCounts[number - 1]++;
+        setDrawCounts(newDrawCounts);
+    };
+
   return (
     <div>
-      <h1 className={styles.title}>学    号</h1>
-      <Lottery maxNumber={38} digits={2} />
-      <div>
+        <h1 className={styles.title}>学    号</h1>
+        <Lottery maxNumber={38} digits={2} />
+        <div>
         <span className={styles.navLink} onClick={handleClick}>
           去抽一个小组
         </span>
-      </div>
-      <div className={styles.floatingText}>离中考还有 {daysUntilExam} 天</div>
+        </div>
+        <div className={styles.floatingText}>离中考还有 {daysUntilExam} 天</div>
+        <div className={styles.floatingTable}>
+            <table>
+                <tbody>
+                {drawCounts.map((count, index) => (
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{count}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     </div>
   );
 }
